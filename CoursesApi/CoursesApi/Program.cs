e9f845bb28baca97f286ca97ebe220ef82cab042
Karol Węgrzyn <karolwg19@gmail.com>
2022-03-15 09:30:37 +0100

    create and configure project

---
+++ CoursesApi/CoursesApi/Program.cs
@@ -0,0 +1,15 @@
+namespace CoursesApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        createHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder createHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
+}
